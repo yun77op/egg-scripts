60bda2e8b2a28d4acdc820efdf4865639b1c9410
Yunhu Jiang <yun77op@163.com>
2018-09-13 10:50:47 +0800

    Improve: exit with failed code when uncaughtException event is caught

--- bin/egg-scripts.js
+++ bin/egg-scripts.js
@@ -2,6 +2,13 @@
 
 'use strict';
 
+process.once('uncaughtException', err => {
+  console.error(err.message);
+  console.error(err.stack);
+
+  process.exitCode = 2;
+});
+
 const Command = require('..');
 
 new Command().start();
