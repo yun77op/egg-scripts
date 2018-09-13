e2c338b20369c00ded4d841952f2493449a6b9ba
Yunhu Jiang <yun77op@163.com>
2018-09-13 11:30:55 +0800

    f

--- bin/egg-scripts.js
+++ bin/egg-scripts.js
@@ -2,13 +2,20 @@
 
 'use strict';
 
-process.once('uncaughtException', err => {
+// process.once('uncaughtException', err => {
+//   console.error(err.message);
+//   console.error(err.stack);
+
+//   process.exitCode = 1;
+// });
+
+try {
+  const Command = require('..');
+
+  new Command().start();
+} catch(err) {
   console.error(err.message);
   console.error(err.stack);
 
   process.exitCode = 1;
-});
-
-const Command = require('..');
-
-new Command().start();
+}
